427587badd28543e01828bc16e2b1f9970217b82
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-01-25 19:17:40 +0330

    commit

--- src/components/emailVerify/landing.js
+++ src/components/emailVerify/landing.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { notify } from "react-notify-toast";
 import Spinner from "./Spinner";
+import "./landing.css";
+import logo_splash from "../../Images/logo_splash.svg";
 //import { API_URL } from "../config";
 
 export default class Landing extends Component {
@@ -47,35 +49,52 @@ export default class Landing extends Component {
     return (
       // A ref is put on the form so that it can be reset once the submission
       // process is complete.
-      <form onSubmit={this.onSubmit} ref={form => (this.form = form)}>
-        <div>
-          <input
-            type="email"
-            name="email"
-            ref={input => (this.email = input)}
-            required
-          />
-          {/*
+      <div className="App">
+        <div className="container">
+          <div className="top-bar">
+            <img src={logo_splash} alt="yop" />
+            <form
+              className="box"
+              onSubmit={this.onSubmit}
+              ref={form => (this.form = form)}
+            >
+              <p className="reset-h2">Email Verify</p>
+              <p className="reset-p">
+                Enter the email address associated with your account, and we’ll
+                email you a link to verify your Email
+              </p>
+              <div>
+                <input
+                  className="email"
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  ref={input => (this.email = input)}
+                  required
+                />
+                {/*
             Putting the label after the input allows for that neat transition
             effect on the label when the input is focused.
           */}
-          <label htmlFor="email">email</label>
-        </div>
-        <div>
-          {/*
+              </div>
+              <div>
+                {/*
             While the email is being sent from the server, provide feedback that
             something is happening by disabling the button and showing a
             <Spinner /> inside the button with a smaller 'size' prop passed in.
           */}
-          <button type="submit" className="btn" disabled={sendingEmail}>
-            {sendingEmail ? (
-              <Spinner size="lg" spinning="spinning" />
-            ) : (
-              "Let's Go!"
-            )}
-          </button>
+                <button type="submit" name="Button" disabled={sendingEmail}>
+                  {sendingEmail ? (
+                    <Spinner size="lg" spinning="spinning" />
+                  ) : (
+                    "Let's Go!"
+                  )}
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </form>
+      </div>
     );
   }
 }
