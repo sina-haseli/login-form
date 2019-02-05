48b6ae0f3af7af9a98eb9812eecd626722032945
Sina <sina.haseli@gmail.com>
2019-02-05 16:40:33 +0330

    fix redux email verify

--- src/components/reset/reset.js
+++ src/components/reset/reset.js
@@ -1,41 +1,72 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  requestToken,
+  resetForgotForm,
+  setForgotFormEmail
+} from "./reset.action";
 import "./reset.css";
 import logo_splash from "../../Images/logo_splash.svg";
 
 class reset extends Component {
+  componentWillUnMount() {
+    this.props.resetForgotForm();
+  }
+  handleEmailChange = e => {
+    this.props.setForgotFormEmail(e.target.value);
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.requestToken(this.props.email);
+  };
+  generateForm() {
+    return (
+      <form className="box" method="post" onSubmit={this.handleSubmit}>
+        <p className="reset-h2">Reset Password</p>
+        <p className="reset-p">
+          Enter the email address associated with your account, and we’ll email
+          you a link to reset your password
+        </p>
+        <input
+          //defaultValue={this.state.email}
+          type="text"
+          name="email"
+          className="email"
+          placeholder="Email Address"
+          onChange={this.handleEmailChange}
+        />
+        <input
+          type="submit"
+          name="Button"
+          //onClick={e => this.handleClick(e)}
+        />
+      </form>
+    );
+  }
+  generateSuccessMessage() {
+    return <Link to={"/confirm"} />;
+  }
   render() {
     return (
       <div className="App">
         <div className="container">
           <div className="top-bar">
             <img src={logo_splash} alt="yop" />
-            <form className="box" method="post">
-              <p className="reset-h2">Reset Password</p>
-              <p className="reset-p">
-                Enter the email address associated with your account, and we’ll
-                email you a link to reset your password
-              </p>
-              <input
-                //defaultValue={this.state.email}
-                type="text"
-                name="email"
-                className="email"
-                placeholder="Email Address"
-              />
-              <Link to="/confirm">
-                <input
-                  type="submit"
-                  name="Button"
-                  //onClick={e => this.handleClick(e)}
-                />
-              </Link>
-            </form>
+            {this.props.emailsent
+              ? this.generateSuccessMessage()
+              : this.generateForm()}
           </div>
         </div>
       </div>
     );
   }
 }
-
-export default reset;
+const mapStateToProps = state => ({
+  email: state.forgotForm.email,
+  emailsent: state.forgotForm.emailsent
+});
+export default connect(
+  mapStateToProps,
+  { requestToken, resetForgotForm, setForgotFormEmail }
+)(reset);
--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import login from "./components/login/loginForm.reducer";
 import landing from "./components/emailVerify/landing.reducer";
+import forgotForm from "./components/reset/reset.reducer";
 
 const reducers = combineReducers({
   login,
-  landing
+  landing,
+  forgotForm
 });
 
 export default reducers;
