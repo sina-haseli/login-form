f41a6517129d4355e125a4945c3a5d5bf450577d
Sina <sina.haseli@gmail.com>
2019-02-09 15:56:02 +0330

    fix Date show on dashboard

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,15 +2,43 @@ import React, { Component } from "react";
 import "./header.css";
 import admin from "../../Images/user-circle-o.svg";
 import logo from "../../Images/logo_splash.svg";
+import { EnhancerOptions as date } from "redux-devtools-extension";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    let today = new Date();
+    let date =
+      monthNames[today.getMonth()] +
+      " " +
+      today.getDate() +
+      "," +
+      today.getFullYear();
+    this.state = {
+      date
+    };
+  }
   render() {
     return (
       <div className="header">
         <img src={logo} alt="yop" className="logo" />
 
         <div className="header-right">
-          <p>April 12,2019</p>
+          <p>{this.state.date}</p>
           <img src={admin} alt="he" className="admin" />
         </div>
       </div>
