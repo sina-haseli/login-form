e4cf686c65ff732779a674e9cb37ea3356b42f11
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-02-16 21:39:49 +0330

    fix tpyo

--- src/components/mainPage/mainPage.jsx
+++ src/components/mainPage/mainPage.jsx
@@ -17,10 +17,26 @@ class MainPage extends Component {
                         <img src={home} />
                         <h4>• Dashboard</h4>
                         <ul>
-                            <li className="li1">Organization</li>
-                            <li className="li2">Staff</li>
-                            <li className="li3">Patients</li>
-                            <li className="li4">Data Logs</li>
+                            <li className="li1">
+                                123<br/>
+                                <hr/>
+                                <br/>
+                                <h3>Organization</h3></li>
+                            <li className="li2">
+                                10<br/>
+                                <hr/>
+                                <br/>
+                                <h3>Staff</h3></li>
+                            <li className="li3">
+                                6<br/>
+                                <hr/>
+                                <br/>
+                                <h3>Patients</h3></li>
+                            <li className="li4">
+                                7<br/>
+                                <hr/>
+                                <br/>
+                                <h3>Data Logs</h3></li>
                         </ul>
                     </div>
                 </div>
