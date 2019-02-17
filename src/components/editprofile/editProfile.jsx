f7f6fc4f158ec843945b226fef3036d0d36d7158
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-02-17 17:52:20 +0330

    fix tpyo

--- src/components/editprofile/editProfile.jsx
+++ src/components/editprofile/editProfile.jsx
@@ -16,11 +16,25 @@ class MainPage extends Component {
                     <div className="content">
                         <h1>Dashboard</h1>
                         <img src={home} />
-                        <h4>• Dashboard</h4>
+                        <h4>• Admin • My Profile</h4>
                             <div className="adminClass">
                                 <h1 className="adminName">Mike Smith</h1>
                                 <p>- [Super Admin]</p>
-                                <Button>edit</Button>
+                                <Button>Edit</Button><br/>
+                                <hr/>
+                                <div className="adminInform">
+                                    <p className="p1">First Name</p>
+                                    <span className="span1">asd</span>
+                                    <p className="p2">Email Address</p>
+                                    <span className="span2">asd</span>
+                                    <p className="p3">Last Name</p>
+                                    <span className="span3">asd</span>
+                                    <p className="p4">Password</p>
+                                    <span className="span4">asd</span>
+                                    <p className="p5">Administration Level</p>
+                                    <span className="span5">asd</span>
+
+                                </div>
                             </div>
                     </div>
                 </div>
