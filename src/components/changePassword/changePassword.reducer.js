b3f2cfbe3d807a99331ba7ae4ae92ab61b59604d
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-02-20 13:40:03 +0330

    fix tpyo

--- src/components/changePassword/changePassword.reducer.js
+++ src/components/changePassword/changePassword.reducer.js
@@ -2,12 +2,49 @@ const RESET_RESET_FORM = "RESET_RESET_FORM";
 const SET_RESET_FORM_ERROR = "SET_RESET_FORM_ERROR";
 const SET_RESET_FORM_CONFIRM_PASSWORD = "SET_RESET_FORM_CONFIRM_PASSWORD";
 const SET_RESET_FORM_PASSWORD = "SET_RESET_FORM_PASSWORD";
+const SET_RESET_FORM_CURRENT_PASSWORD = "SET_RESET_FORM_CURRENT_PASSWORD";
 
-
-const initialState={
-    confirmPassword: '',
-    current_password:'',
-    new_password:''
+const initialState = {
+  confirm_password: "",
+  current_password: "",
+  new_password: ""
 };
 
-export default (state=)
+export default (state = initialState, action) => {
+  const newState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RESET_RESET_FORM:
+      return initialState;
+    case SET_RESET_FORM_ERROR:
+      const actionErrors = action.error.errors;
+      const actionMessage = action.error.message;
+      newState.error = Object.assign({}, initialState.error);
+
+      const newFields = [];
+      const newMessages = [];
+      if (actionErrors) {
+        for (let key in actionErrors) {
+          newFields.push(actionErrors[key].name);
+          newMessages.push(actionErrors[key].message);
+        }
+        newState.error.fields = [newFields];
+        newState.error.messages = [...newMessages];
+      } else {
+        newState.error.messages = [actionMessage];
+      }
+      break;
+    case SET_RESET_FORM_CONFIRM_PASSWORD:
+      newState.confirm_password = action.confirmPassword;
+      break;
+    case SET_RESET_FORM_PASSWORD:
+      newState.new_password = action.password;
+      break;
+    case SET_RESET_FORM_CURRENT_PASSWORD:
+      newState.current_password = action.currentPassword;
+      break;
+    default:
+      return state;
+  }
+  return newState;
+};
