f746536193585567e8bd665135bea31ea109ce3a
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-02-20 13:25:42 +0330

    fix tpyo

--- src/components/changePassword/changePassword.actions.js
+++ src/components/changePassword/changePassword.actions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { _setEmailError } from "../emailVerify/landing.action";
 //import {updateStoreWithUser} from '../User.actions';
 
 const RESET_RESET_FROM = "RESET_RESET_FORM";
@@ -9,3 +10,34 @@ const SET_RESET_FORM_PASSWORD = "SET_RESET_FORM_PASSWORD";
 const _resetResetForm = () => ({
   type: RESET_RESET_FROM
 });
+
+const _setResetFormError = error => ({
+  type: SET_RESET_FROM_ERROR,
+  error
+});
+
+export const setResetFormConfirmPassword = confirmPassword => ({
+  type: SET_RESET_FROM_CONFIRM_PASSWORD,
+  confirmPassword
+});
+
+export const setResetFormPassword = password => ({
+  type: SET_RESET_FORM_PASSWORD,
+  password
+});
+
+export const changeResetFormPassword = (password, token) => dispatch =>
+  axios
+    .post(`/api/reset/${token}`, { password })
+    .then(() => {
+      dispatch(_resetResetForm());
+    })
+    .catch(err => {
+      dispatch(_setResetFormError(err.response.data.error));
+    });
+export const verifyToken = token => dispatch =>
+  axios.get(`api/reset/${token}`).catch(err => {
+    if (err.response.status === 400) {
+      dispatch(_setResetFormError(err.response.data.error));
+    }
+  });
