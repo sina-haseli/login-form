20e3588ab30128cd2ff3ec83bdc2fe48417362a5
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-02-27 15:46:58 +0330

    fix tpyo

--- src/components/editprofile/editProfile.reducer.js
+++ src/components/editprofile/editProfile.reducer.js
@@ -1,2 +1,10 @@
 import axios from "axios";
 //import {updateStoreWithUser} from '../User.actions';
+
+const SET_PROFILE_FIRSTNAME = "SET_PROFILE_FIRSTNAME";
+const SET_PROFILE_LASTNAME = "SET_PROFILE_LASTNAME";
+const SET_PROFILE_EMAIL = "SET_PROFILE_EMAIL";
+const SET_PROFILE_PASSWORD = "SET_PROFILE_PASSWORD";
+const SET_PROFILE_ERROR = "SET_PROFILE_ERROR";
+
+//action creators
