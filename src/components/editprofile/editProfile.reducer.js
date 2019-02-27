aad5cc34959598eba3800cd29415bb6e577534e9
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-02-27 15:52:03 +0330

    fix tpyo

--- src/components/editprofile/editProfile.reducer.js
+++ src/components/editprofile/editProfile.reducer.js
@@ -8,3 +8,28 @@ const SET_PROFILE_PASSWORD = "SET_PROFILE_PASSWORD";
 const SET_PROFILE_ERROR = "SET_PROFILE_ERROR";
 
 //action creators
+
+export const setProfileFirstName = firstname => ({
+  type: SET_PROFILE_FIRSTNAME,
+  firstname
+});
+
+export const setProfileLastName = lastname => ({
+  type: SET_PROFILE_LASTNAME,
+  lastname
+});
+
+export const setProfileEmail = email => ({
+  type: SET_PROFILE_FIRSTNAME,
+  email
+});
+
+export const setProfilePassword = password => ({
+  type: SET_PROFILE_FIRSTNAME,
+  password
+});
+
+export const setProfileError = error => ({
+  type: SET_PROFILE_FIRSTNAME,
+  error
+});
