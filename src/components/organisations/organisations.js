655ec836c7bbcb93bddcabd218ba2b7c547620a7
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-03-04 15:49:32 +0330

    fix tpyo

--- src/components/organisations/organisations.js
+++ src/components/organisations/organisations.js
@@ -3,6 +3,7 @@ import Header from "../header/header";
 import Sider from "../sider/sider";
 import home from "../../Images/dash_home_bread_crumb.svg";
 import { Button } from "antd";
+import "./organisations.css";
 
 class Organisations extends Component {
   render() {
@@ -17,22 +18,9 @@ class Organisations extends Component {
             <h4>• Organisations</h4>
             <div className="adminClass">
               <h1 className="adminName" />
-              <p>- [Super Admin]</p>
               <Button>Edit</Button>
               <br />
               <hr />
-              <div className="adminInform">
-                <p className="p1">First Name</p>
-                <span className="span1">asd</span>
-                <p className="p2">Email Address</p>
-                <span className="span2">dsa</span>
-                <p className="p3">Last Name</p>
-                <span className="span3">asd</span>
-                <p className="p4">Password</p>
-                <span className="span4">dsa</span>
-                <p className="p5">Administration Level</p>
-                <span className="span5">asd</span>
-              </div>
             </div>
           </div>
         </div>
