795fc8f8ce872b202fefc067303dd723d9dd07da
sina-haseli <40861222+sina-haseli@users.noreply.github.com>
2019-03-04 16:15:32 +0330

    fix tpyo

--- src/components/organisations/organisations.js
+++ src/components/organisations/organisations.js
@@ -4,8 +4,39 @@ import Sider from "../sider/sider";
 import home from "../../Images/dash_home_bread_crumb.svg";
 import { Button } from "antd";
 import "./organisations.css";
+import { Table } from "antd";
+//import reqwest from "reqwest";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    sorter: true,
+    render: name => `${name.first} ${name.last}`,
+    width: "20%"
+  },
+  {
+    title: "Gender",
+    dataIndex: "gender",
+    filters: [
+      { text: "Male", value: "male" },
+      { text: "Female", value: "female" }
+    ],
+    width: "20%"
+  },
+  {
+    title: "Email",
+    dataIndex: "email"
+  }
+];
 
 class Organisations extends Component {
+  state = {
+    data: [],
+    pagination: {},
+    loading: false
+  };
+
   render() {
     return (
       <div className="app">
@@ -21,6 +52,14 @@ class Organisations extends Component {
               <Button>Add Organisation</Button>
               <br />
               <hr />
+              <Table
+                columns={columns}
+                //rowKey={record => record.login.uuid}
+                dataSource={this.state.data}
+                pagination={this.state.pagination}
+                loading={this.state.loading}
+                //onChange={this.handleTableChange}
+              />
             </div>
           </div>
         </div>
